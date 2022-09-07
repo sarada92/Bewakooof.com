8ba6d533c6f101987ed2e73bc3e00c8fbc871e74
Sarada Rout <srout@groupon.com>
2022-09-07 17:33:28 +0530

    setting update

--- ecom/urls.py
+++ ecom/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
-    path('', api.views.home)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
